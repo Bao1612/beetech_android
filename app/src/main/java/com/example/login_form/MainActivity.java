0ff33c7b20cc506db1e86996597fa6ec66586365
baole <bao16122001@gmail.com>
2022-12-18 14:47:43 +0700

    [WIP]API-Login

--- app/src/main/java/com/example/login_form/MainActivity.java
+++ app/src/main/java/com/example/login_form/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.login_form;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
         loginbtn = findViewById(R.id.loginbtn);
 
         loginbtn.setOnClickListener(view -> {
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            if(username.getText().toString().equals("baole") && password.getText().toString().equals("baole")) {
                 Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                Intent navbar = new Intent(this, Navigator.class);
+                startActivity(navbar);
             } else {
                 Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
             }
